10bf9702a63667e84dccd352986fc0e3f193cd72
TDashner <das21007@byui.edu>
2025-01-27 10:13:57 -0700

    first commit

---
+++ dom-basics.js
@@ -0,0 +1,16 @@
+const newParagraph = document.createElement("p");
+newParagraph.innerText = "Added with Javascript!";
+document.body.appendChild(newParagraph);
+
+const image = document.createElement("img");
+image.setAtrribute("src", "https://picsum.photos/200");
+image.setAtrribute("alt", "random image");
+document.body.appendChild(image);
+
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
+document.body.appendChild(newDiv);
+
+const section = document.createElement("section");
+section.innerHTML = "<h2>DOM Basics</h2><p>This was added through Javascript</p>";
+document.body.appendChild(section);
---
+++ running.js
@@ -0,0 +1,4 @@
+const apple = 5;
+const oranges = 3;
+let total = apples + oranges;
+console.log("total:", total);
