5249334d7ffd588c333d30425259da0cb7606316
TDashner <das21007@byui.edu>
2025-01-27 10:25:49 -0700

    logo

--- mission/mission.js
+++ mission/mission.js
@@ -1,7 +1,16 @@
 let selectElem = document.querySelector("select");
+
 function changeTheme(){
-    let logo = document.querySelector("image");
-    console.log('in funvction');
+    let logo = document.querySelector("img");
+    let current = selectElem.value;
+    if (current == 'dark') {
+        document.body.className = 'dark';
+        logo.setAttribute('src', 'byui-logo_white.png');
+    } else {
+        document.body.classList.remove('dark');
+        logo.setAttribute('src', 'byui-logo_blue.webp');
+    }
+
 
 }
 
